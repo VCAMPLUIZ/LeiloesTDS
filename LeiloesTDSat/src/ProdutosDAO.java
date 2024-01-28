9db08f40eb7a497012b732aee8a6faa84d25b95b
Victor <Vluiz528@gmail.com>
2024-01-28 15:31:30 -0300

    Funçao cadastrarProduto() criada e implementada em cadastroVIEW

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -12,6 +12,7 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
@@ -22,12 +23,39 @@ public class ProdutosDAO {
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+ public void cadastrarProduto(ProdutosDTO produto) {
+
+        try {
+            conectaDAO conexao = new conectaDAO();
+            conexao.connectDB();
+            String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+
+            PreparedStatement consulta = conexao.getConexao().prepareStatement(sql, new String[]{"ID"});
+
+            consulta.setString(1, produto.getNome());
+            consulta.setInt(2, produto.getValor());
+            consulta.setString(3, produto.getStatus());
+
+            int status = consulta.executeUpdate();
+
+            if (status == 1) {
+                ResultSet generatedKeys = consulta.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    int idGerado = generatedKeys.getInt(1);
+                    JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso");
+                    System.out.println("Produto cadastrado com sucesso. ID gerado: " + idGerado);
+                } else {
+                    System.out.println("Erro ao recuperar o ID gerado.");
+                }
+            } else {
+                System.out.println("Erro ao cadastrar o produto.");
+            }
+
+            conexao.desconectar();
+
+        } catch (SQLException ex) {
+            System.out.println("Erro ao cadastrar dados: " + ex.getMessage());
+        }
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
